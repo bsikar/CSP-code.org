67b676a4f9bced0671776e395285ecb912d42258
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-01-26 07:50:30 -0600

    Update lesson8-3.js

--- unit5/lesson8-3.js
+++ unit5/lesson8-3.js
@@ -1,12 +1,5 @@
 // List of icons to randomly choose between
-var iconsList = ["icon://fa-heart", 
-                 "icon://fa-music", 
-                 "icon://fa-smile-o", 
-                 "icon://fa-globe", 
-                 "icon://fa-tree", 
-                 "icon://fa-bolt", 
-                 "icon://fa-moon-o",
-                 "icon://fa-star"];
+var iconsList = ["icon://fa-heart", "icon://fa-music", "icon://fa-smile-o", "icon://fa-globe", "icon://fa-tree", "icon://fa-bolt", "icon://fa-moon-o", "icon://fa-star"];
 // What icon index will be shown
 var icon = randomNumber(0, iconsList.length-1);
 
@@ -23,7 +16,7 @@ function updateScreen(str) {
   // if the color button was pressed change the color of the icons as well as the background
   if (str == "color") {
     for (var i = 0; i < 20; ++i) {
-      setProperty("icon" + i, "icon-color", rgb(randomNumber(0,255), randomNumber(0,255) ,randomNumber(0,255), Math.random()));
+      setProperty("icon" + i, "icon-color", rgb(randomNumber(0,255), randomNumber(0,255), randomNumber(0,255), Math.random()));
       setProperty("homeScreen", "background-color", rgb(randomNumber(0,255), randomNumber(0,255), randomNumber(0,255), 0.5));
     }
     // if the location button was pressed change the location of all of the icons
