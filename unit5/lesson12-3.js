f87d56f53f16ff3e588b9ad838d1212abbe48462
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-02-03 07:22:05 -0600

    Update lesson12-3.js

--- unit5/lesson12-3.js
+++ unit5/lesson12-3.js
@@ -6,11 +6,16 @@ var info = {
   low: getColumn("Daily Weather", "Low Temperature"),
   condition: getColumn("Daily Weather", "Condition Description"),
   icon: getColumn("Daily Weather", "Icon"),
+  day: getColumn("Daily Weather", "Forecast Number"),
 };
 
 // if someone clicks the "Get Forecast" Button
 onEvent("forecastButton", "click", function() {
-  var x = randomNumber(0, 600);
+  var x = 0;
+
+  while (info.day[x] != 2) {
+    x = randomNumber(0, 600);
+  }
   setProperty("cityOutput", "text", info.city[x]);
   setProperty("highTempOutput", "text", info.high[x]);
   setProperty("lowTempOutput", "text", info.low[x]);
